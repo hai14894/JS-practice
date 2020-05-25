52691fff25354b8d02a51dd7b0d269d8e3bc5ab0
hai14894 <hohoanghai14894@gmail.com>
2020-05-25 16:51:05 +1000

    first commit

---
+++ code.js
@@ -0,0 +1,22 @@
+let user = 'hai'
+if (user ==='tom') {
+    console.log(`hi Tom`);
+} else {
+    console.log(`hello someone other than Tom`);
+}
+
+let age = prompt(`what's your age?`)
+
+if (age >=18) {
+    console.log('welcome to the casino');
+} else {
+    alert('deny entry')
+}
+let students =[]
+let a = 1
+while (a <=3){
+students.push(prompt(`what's your name?`))
+console.log(students);
+a +=1
+}
+
