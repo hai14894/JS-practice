3043da89c982c1704985623a67985a2d43fdb683
hai14894 <hohoanghai14894@gmail.com>
2020-07-01 10:16:13 +1000

    conways game of life challenge

---
+++ conways-game-of-life.js
@@ -0,0 +1,24 @@
+let game = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 0],
+    [0, 1, 1, 0, 0],
+    [0, 0, 1, 0, 1],
+    [0, 0, 1, 0, 0]
+]
+function conWaysGameOfLife(game){
+    for(let x = 0; x < game.length; x++){
+        for(let y=0; y < game[x].length; y++){
+        
+            let neighborCells =game[x-1][y-1] + game[x-1][y] + game[x-1][y+1] +game[x][y-1]+game[x][y+1] + game[x+1][y-1] +game[x+1][y] +game[x+1][y+1]
+            if(game[x][y] === 1){
+                if (neighborCells < 2 || neighborCells >3){
+                    game[x][y] = 0
+                }
+            }else if(game[x][y] === 0 && neighborCells === 3){
+                game[x][y] = 1
+            }
+    }
+return game
+}
+}
+conWaysGameOfLife(game)
